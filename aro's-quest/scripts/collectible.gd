a6f424eee31cc1c54acfaf3ee26aff849f9cddf6
Marcus Leano <143433132+BingBucket@users.noreply.github.com>
2025-10-22 19:06:31 -0400

    random stuff

--- aro's-quest/scripts/collectible.gd
+++ aro's-quest/scripts/collectible.gd
@@ -2,8 +2,8 @@ extends Node2D
 @onready var sprite = $Area2D/AnimatedSprite2D
 @onready var timer = $Area2D/Timer
 signal collected
-func ready() -> void:
-	sprite.play()
+func _ready() -> void:
+	sprite.play("idle")
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("collected")
